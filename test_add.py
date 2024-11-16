c874d31f94c1b9371e544a55f7d5d64e5851c793
eldawi <63516988+eldawi@users.noreply.github.com>
2024-11-16 23:43:22 +0200

    test_add.py

--- test_add.py
+++ test_add.py
@@ -1,1 +1,13 @@
+import unittest
+from add import Task
 
+class Testadd(unittest.TestCase):
+  def test_add(self):
+    result = Task.add(2, 8)
+    self.assertEqual(result, 10)
+
+    result = Task.add(4, 4)
+    self.assertEqual(result, 8)
+
+    result = Task.add(6, 3)
+    self.assertEqual(result, 9)
